845d9588aa8f88f70b55d5eaeaa450323d53ba89
Shnayzi <bmtinana@gmail.com>
2024-10-17 22:15:40 -0300

    imagens --> ok BikeNova (criação de objeto, construtor de classe e métodos de acesso, etc) --> ok

--- app/src/main/java/br/com/etecia/myapp/BikesNovasFragment.java
+++ app/src/main/java/br/com/etecia/myapp/BikesNovasFragment.java
@@ -3,6 +3,7 @@ package br.com.etecia.myapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -28,16 +29,23 @@ public class BikesNovasFragment extends Fragment {
 
         //carregando os objetos na lista
         lstBikeNova = new ArrayList<>();
-        lstBikeNova.add(new BikeNova("Bike", R.drawable.bike1))
-        lstBikeNova.add(new BikeNova("Bike 2", R.drawable.bike2))
-        lstBikeNova.add(new BikeNova("Bike 3", R.drawable.bike3))
-        lstBikeNova.add(new BikeNova("Bike 4", R.drawable.bike4))
-        lstBikeNova.add(new BikeNova("Bike 5", R.drawable.bike5))
-        lstBikeNova.add(new BikeNova("Bike 6", R.drawable.bike6))
-        lstBikeNova.add(new BikeNova("Bike 7", R.drawable.bike7))
-        lstBikeNova.add(new BikeNova("Bike 8", R.drawable.bike8))
-        lstBikeNova.add(new BikeNova("Bike 9", R.drawable.bike9))
-        lstBikeNova.add(new BikeNova("Bike 10", R.drawable.bike10))
+        lstBikeNova.add(new BikeNova("Bike", R.drawable.bike1));
+        lstBikeNova.add(new BikeNova("Bike 2", R.drawable.bike2));
+        lstBikeNova.add(new BikeNova("Bike 3", R.drawable.bike3));
+        lstBikeNova.add(new BikeNova("Bike 4", R.drawable.bike4));
+        lstBikeNova.add(new BikeNova("Bike 5", R.drawable.bike5));
+        lstBikeNova.add(new BikeNova("Bike 6", R.drawable.bike6));
+        lstBikeNova.add(new BikeNova("Bike 7", R.drawable.bike7));
+        lstBikeNova.add(new BikeNova("Bike 8", R.drawable.bike8));
+        lstBikeNova.add(new BikeNova("Bike 9", R.drawable.bike9));
+        lstBikeNova.add(new BikeNova("Bike 10", R.drawable.bike10));
+
+        //carregando o adaptador
+        RecyclerAdapterBikeNova adapterBikeNova = new RecyclerAdapterBikeNova(getContext(),lstBikeNova);
+
+        idRecBikeNovas.setLayoutManager(new GridLayoutManager(getContext(),2));
+
+        idRecBikeNovas.setAdapter(adapterBikeNova);
 
         return view;
 
